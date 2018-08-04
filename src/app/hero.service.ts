37711e5fa580f942fd677355aec2b2a68e34f1be
Kopp <KoppB@atonix.com>
2018-08-03 22:20:12 -0500

    Swapped out the heroes mock service for a real one

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,26 +1,50 @@
 import { MessageService } from './message.service';
 import {Observable, of} from 'rxjs';
 
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
+
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
+  private heroesUrl = 'api/heroes';
 
-  constructor(private messageService: MessageService) { }
+  constructor(
+    private messageService: MessageService,
+    private http: HttpClient
+  ) { }
 
   getHeroes(): Observable<Hero[]> {
-    this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+    return this.http.get<Hero[]>(
+      this.heroesUrl
+    ).pipe(
+      tap(heroes => this.log('fetched heroes')),
+      catchError(this.handleError('getHeroes', []))
+    );
   }
 
   getHero(id: number): Observable<Hero> {
-    this.messageService.add(
-      `HeroService: fetched hero id=${id}`
+    return this.http.get<Hero>(
+      `${this.heroesUrl}/${id}`
+    ).pipe(
+      tap(_ => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>('getHero'))
     );
-    return of(HEROES.find(hero => hero.id == id));
+  }
+
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      this.log(`${operation} failed: ${error.message}`);
+      return of(result as T);
+    }
   }
 }
